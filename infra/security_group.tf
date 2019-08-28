1befafae6dfe558177afb6077e6b356becbe83f0
teitei-tk <teitei.tk@gmail.com>
2019-08-28 23:55:56 +0900

    add security_group.tf

---
+++ infra/security_group.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "app" {
+  name   = "app"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "web"
+  }
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.app.id}"
+}
+
+resource "aws_security_group_rule" "web" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.app.id}"
+}
+
+resource "aws_security_group_rule" "all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.app.id}"
+}
