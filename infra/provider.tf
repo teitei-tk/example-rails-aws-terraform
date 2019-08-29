cb0ca962fe08e14f6bf94f4fafd39472d66765db
teitei-tk <teitei.tk@gmail.com>
2019-08-29 17:24:38 +0900

    backend using s3

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,3 +3,11 @@ provider "aws" {
   access_key = "${var.AWS_ACCESS_KEY}"
   secret_key = "${var.AWS_SECRET_KEY}"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "example-rails-aws-terraform"
+    key    = "terraform.state"
+    region = "ap-northeast-1"
+  }
+}
